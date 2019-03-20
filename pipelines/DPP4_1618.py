59d1a1fdea498d5a0bd9080c55682336badc3161
lelaboratoire <grixor@gmail.com>
2019-03-20 09:11:51 -0400

    add pipelines for CB1 and DPP4

--- pipelines/DPP4_1618.py
+++ pipelines/DPP4_1618.py
@@ -1,14 +1,14 @@
 from sklearn.preprocessing import FunctionTransformer
 from copy import copy
-from sklearn.decomposition import PCA
-from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.feature_selection import VarianceThreshold
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
 def opt_pipe(training_features, testing_features):
 	exported_pipeline = make_pipeline(
-    SelectPercentile(score_func=f_regression, percentile=34),
-    PCA(iterated_power=2, svd_solver="randomized"),
-    MLPRegressor(activation="relu", alpha=10.0, learning_rate="adaptive", learning_rate_init=0.01, momentum=0.75, solver="sgd")
+    VarianceThreshold(threshold=0.05),
+    MinMaxScaler(),
+    MLPRegressor(activation="relu", alpha=1.0, learning_rate="adaptive", learning_rate_init=0.1, momentum=0.5, solver="sgd")
 )
 	return({'train_feat': training_features, 'test_feat': testing_features, 'pipe': exported_pipeline})
