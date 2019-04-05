1f00e51c691c16ead9c40d088dbbe984cb92fcbb
lelaboratoire <grixor@gmail.com>
2019-04-05 11:25:10 -0400

    correct parsing dataset name

--- get_dataset_names.R
+++ get_dataset_names.R
@@ -1,11 +1,11 @@
 rm(list = ls())
 library(tidyverse)
 
-split_names <- list.files("qsar", pattern = "*.csv") %>%
-  strsplit(split = '_') %>%
-  map(1) %>%
-  unlist() %>%
+split_names <- list.files("qsar", pattern = "*.csv")
+
+gsub('_training_preprocessed.csv|_test_preprocessed.csv', '', split_names) %>%
   unique() %>%
   data.frame() %>%
   write_csv('qsar_datasets.txt', col_names = FALSE)
+
 
