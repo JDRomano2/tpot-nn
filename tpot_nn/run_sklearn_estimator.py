1c9913466ec26b7db40e3726153c3ee74500fbdb
Joseph Romano <jdromano2@gmail.com>
2020-01-23 10:58:06 -0500

    Use more constrained configuration in sklearn LR

--- tpot_nn/run_sklearn_estimator.py
+++ tpot_nn/run_sklearn_estimator.py
@@ -10,9 +10,8 @@ from sklearn.datasets import load_breast_cancer
 
 tpot_config = {
     'sklearn.linear_model.LogisticRegression': {
-        'penalty': ["l1", "l2"],
-        'C': [1e-4, 1e-3, 1e-2, 1e-1, 0.5, 1., 5., 10., 15., 20., 25.],
-        'dual': [True, False]
+        'solver': ['sag'],
+        'penalty': ['none']
     }
 }
 
