f05e20d3635173d450c18dbdf77d89449d3511d8
Joseph Romano <jdromano2@gmail.com>
2020-03-02 10:54:49 -0500

    Turn on automatic job submission in job_builder.py

--- job_builder.py
+++ job_builder.py
@@ -44,18 +44,4 @@ for dset in dsets:
       ])
       jobfile.close()
 
-      #os.system('bsub < ' + jobfile_path)
-
-# Example:
-# def submitJob(uniquejobname,scratchpath,logpath,runpath,dataset,outfile,algorithm,discthresh,outcomelabel,neighbors):
-#     """ Submit Job to the cluster. """
-#     jobName = scratchpath+'/'+uniquejobname+'_'+str(time.time())+'_run.sh'
-#     shFile = open(jobName, 'w')
-#     shFile.write('#!/bin/bash\n')
-#     shFile.write('#BSUB -J '+uniquejobname+'_'+str(time.time())+'\n')
-#     shFile.write('#BSUB -o ' + logpath+'/'+uniquejobname+'.o\n')
-#     shFile.write('#BSUB -M ' + 10000'+'\n')
-#     shFile.write('#BSUB -e ' + logpath+'/'+uniquejobname+'.e\n\n')
-#     shFile.write('python '+runpath+'/'+'run_scikit-rebate.py '+str(dataset)+' '+str(outfile)+' '+str(algorithm)+' '+str(discthresh)+' '+str(outcomelabel)+' '+str(neighbors)+'\n')
-#     shFile.close()
-#     os.system('bsub < '+jobName)
+      os.system('bsub < ' + jobfile_path)
